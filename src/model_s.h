463a20bcf5cdaf14aff4dd846b9cafeb05fd8346
caiqifang <cai2053@gmail.com>
2016-11-15 14:07:59 -0500

    writing class

--- src/model_s.h
+++ src/model_s.h
@@ -1,3 +1,11 @@
+// this is the sequantial implementation of the binomial pricing model
 
+class ModelS{
+    private:
+        int timePeriod;
 
+    public:
+        virtual ModelS(int period);
+        virtual ~ModelS() {};
+};
 
