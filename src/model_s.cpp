c8c7b379308f336caf9d03f218bed50af5d1d2ae
caiqifang <cai2053@gmail.com>
2016-11-16 15:44:18 -0500

    creating the sequantial model

--- src/model_s.cpp
+++ src/model_s.cpp
@@ -1,3 +1,47 @@
+#include <model_s.h>
+#include <cmath>
+#include <cfenv>
+#include <stdexcept>
+#include <stdlib.h>
 
+ModelS::ModelS(int period, double k){
+    if(period > MAXPERIOD) timePeriod = MAXPERIOD;
+    else timePeriod = period;
+    strike = k;
+    totalStage = (timePeriod + RECURLENGTH - 1)/ RECURLENGTH;
+}
 
+double ModelS::safePower(double base, int power){
+    std::feclearexcept(FE_OVERFLOW);
+    std::feclearexcept(FE_UNDERFLOW); // overflow and underflow detection
+    double ret = pow(base, power);
+    if((bool)std::fetestexcept(FE_OVERFLOW) || (bool)std::fetestexcept(FE_UNDERFLOW)){
+        throw std::invalid_argument("power raise exception!\n");
+    }
+    return ret;
+}
 
+double ModelS::worker(double p, double q, double r, double s, double u, double d,
+                    int startPeriod, int stopPeriod){
+  // recursive
+  // backward induction
+
+}
+
+double ModelS::calculate(double up, double down, double rate, double s0){
+    double result;
+    // Allocate buffer
+    buffer = (double*) calloc(1<<(MAXPERIOD - RECURLENGTH),sizeof(double));
+    int currentStage;
+    double p = (1 + rate - down) /(up - down);
+    double q = (up - rate - 1) /(up - down);
+    for (int i = 0; i < totalStage; i++){
+        currentStage = totalStage - i;
+
+    }
+    return result;
+}
+
+ModelS::~ModelS(){
+    free(buffer);
+}
--- src/model_s.h
+++ src/model_s.h
@@ -1,11 +1,27 @@
 // this is the sequantial implementation of the binomial pricing model
 
+#define RECURLENGTH 10
+#define MAXPERIOD 30
+
 class ModelS{
     private:
-        int timePeriod;
+        int timePeriod; // n period of time step
+
+        int totalStage; // break the state process to reduce memory consumed
+
+        double strike; // strike price  -> we are modeling a call option
+
+        double *buffer; // intermediate result buffer
 
     public:
-        virtual ModelS(int period);
-        virtual ~ModelS() {};
-};
+        virtual ModelS(int period, double k);
 
+        virtual double safePower(double base, int power) = 0;
+
+        virtual double worker(double p, double q, double r, double s,
+                double u, double d, int startPeriod, int stopPeriod) = 0;
+
+        virtual double calculate(double up, double down, double rate, double s0) = 0;
+
+        virtual ~ModelS();
+};
