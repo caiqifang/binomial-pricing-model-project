c0cea45694baa14459f3156eae39edc4665d80e9
caiqifang <cai2053@gmail.com>
2016-12-03 13:00:40 -0500

    new file

---
+++ src/cudaModel.cu
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <math.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+
+#include "cudaModel.h"
+/*
+  This file is the implementation of parallel binomial model
+   */
+
---
+++ src/cudaModel.h
@@ -0,0 +1,9 @@
+#ifndef __CUDA_MODEL_H__
+#define __CUDA_MODEL_H__
+
+
+
+
+
+
+#endif
